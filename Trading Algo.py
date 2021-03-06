fe117b692c2e1cf0e6192e9dbea32e9c35340c61
g-i-joe-oh-six <62847749+g-i-joe-oh-six@users.noreply.github.com>
2021-03-06 16:20:46 -0500

    Add files via upload

---
+++ Trading Algo.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import requests
+import xlsxwriter
+import math
+
+
+# In[2]:
+
+
+stocks = pd.read_csv(r"C://Users//mjlig/Downloads/sp_500_stocks.csv")
+stocks
+
+
+# In[3]:
+
+
+from secrets import IEX_CLOUD_API_TOKEN
+
+
+# In[4]:
+
+
+symbol='AAPL'
+api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+data = requests.get(api_url).json()
+data
+
+
+# In[5]:
+
+
+data['latestPrice']
+data['marketCap']
+
+
+# In[6]:
+
+
+my_columns = ['Ticker', 'Price','Market Capitalization', 'Number Of Shares to Buy']
+final_dataframe = pd.DataFrame(columns = my_columns)
+final_dataframe
+
+
+# In[7]:
+
+
+final_dataframe = final_dataframe.append(
+                                        pd.Series(['AAPL', 
+                                                   data['latestPrice'], 
+                                                   data['marketCap'], 
+                                                   'N/A'], 
+                                                  index = my_columns), 
+                                        ignore_index = True)
+final_dataframe
+
+
+# In[9]:
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+# In[10]:
+
+
+symbol_groups = list(chunks(stocks['Ticker'], 100))
+symbol_strings = []
+for i in range(0, len(symbol_groups)):
+    symbol_strings.append(','.join(symbol_groups[i]))
+#     print(symbol_strings[i])
+
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+for symbol_string in symbol_strings:
+#     print(symbol_strings)
+    batch_api_call_url = f'https://sandbox.iexapis.com/stable/stock/market/batch/?types=quote&symbols={symbol_string}&token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(batch_api_call_url).json()
+    for symbol in symbol_string.split(','):
+        final_dataframe = final_dataframe.append(
+                                        pd.Series([symbol, 
+                                                   data[symbol]['quote']['latestPrice'], 
+                                                   data[symbol]['quote']['marketCap'], 
+                                                   'N/A'], 
+                                                  index = my_columns), 
+                                        ignore_index = True)
+        
+    
+final_dataframe
+
+
+# In[17]:
+
+
+portfolio_size = input("Enter the value of your portfolio:")
+
+try:
+    val = float(portfolio_size)
+except ValueError:
+    print("That's not a number! \n Try again:")
+    portfolio_size = input("Enter the value of your portfolio:")
+
+
+# In[18]:
+
+
+position_size = float(portfolio_size) / len(final_dataframe.index)
+for i in range(0, len(final_dataframe['Ticker'])-1):
+    final_dataframe.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / final_dataframe['Price'][i])
+final_dataframe
+
+
+# In[19]:
+
+
+writer = pd.ExcelWriter('recommended_trades.xlsx', engine='xlsxwriter')
+final_dataframe.to_excel(writer, sheet_name='Recommended Trades', index = False)
+
+
+# In[20]:
+
+
+background_color = '#0a0a23'
+font_color = '#ffffff'
+
+string_format = writer.book.add_format(
+        {
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+dollar_format = writer.book.add_format(
+        {
+            'num_format':'$0.00',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+integer_format = writer.book.add_format(
+        {
+            'num_format':'0',
+            'font_color': font_color,
+            'bg_color': background_color,
+            'border': 1
+        }
+    )
+
+
+# In[21]:
+
+
+column_formats = { 
+                    'A': ['Ticker', string_format],
+                    'B': ['Price', dollar_format],
+                    'C': ['Market Capitalization', dollar_format],
+                    'D': ['Number of Shares to Buy', integer_format]
+                    }
+
+for column in column_formats.keys():
+    writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+    writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+
+
+# In[22]:
+
+
+writer.save()
+
+
+# In[ ]:
+
+
+
+
